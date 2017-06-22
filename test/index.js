401794dd4265caa4655c5129a558c6a544e69111
Alexys <alexys.lameda@quadminds.com>
2017-06-22 10:46:30 -0300

    Tests and npm init

---
+++ test/index.js
@@ -0,0 +1,53 @@
+const expect = require('expect');
+const Concurrency = require('../');
+describe('Promise Concurrency', () => {
+    const simplePromise = () => new Promise((resolve, reject) => {
+        setTimeout(function(){
+            resolve("¡Success!");
+        }, 250);
+    });
+    const promiseWithValue = value => new Promise((resolve, reject) => {
+        setTimeout(function(){
+            resolve(value);
+        }, 250);
+    });
+    const rejectedPromise = () => new Promise((resolve, reject) => {
+        setTimeout(function(){
+            resolve("¡Reject!");
+        }, 250);
+    });
+    it('should resolve max 3 promises at time', done => {
+        const promises = [];
+        for (let i=0; i<10; i++){
+            promises.push(() => simplePromise());
+        }
+        Concurrency.all(promises, 3)
+            .then(() => done())
+            .catch(err => done(err));
+    });
+    it('should return the values with the same order', done => {
+        const promises = [];
+        for (let i=0; i<10; i++){
+            promises.push(() => promiseWithValue(i));
+        }
+        Concurrency.all(promises, 3)
+            .then(results => {
+                results.forEach((value, index) => {
+                    expect(value).toBe(index);
+                });
+                done();
+            })
+            .catch(err => done(err));
+    });
+    it('should throw rejection if some of the promise reject', done => {
+        const promises = [];
+        for (let i=0; i<10; i++){
+            promises.push(() => simplePromise());
+        }
+        promises.push(() => rejectedPromise());
+        Concurrency.all(promises, 3)
+            .then(results => done(results))
+            .catch(err => done());
+    });
+
+});
